eb07b79923ad9b39c8e135a327dc2405f4505363
ck <chunkind@naver.com>
2026-05-09 21:03:29 +0900

    5/9: SwapChain

--- DirectX11Engine/Project/Engine/CDevice.cpp
+++ DirectX11Engine/Project/Engine/CDevice.cpp
@@ -20,6 +20,13 @@ CDevice::~CDevice()
 	m_Device->Release();
 	m_Context->Release();
 	m_SwapChain->Release();
+	//new
+	m_RenderTargetTex->Release();
+
+	//new
+	m_RTV->Release();
+	m_DepthStencilTex->Release();
+	m_DSV->Release();
 }
 
 int CDevice::init(HWND _hWnd, POINT _Resolution)
@@ -40,6 +47,12 @@ int CDevice::init(HWND _hWnd, POINT _Resolution)
 		, &m_Device, &level, &m_Context);
 
 	if (FAILED(CreateSwapchain()))
+	{
+		return E_FAIL;
+	}
+
+	//new
+	if (FAILED(CreateView()))
 	{
 		return E_FAIL;
 	}
@@ -84,6 +97,41 @@ int CDevice::CreateSwapchain()
 	pDXGIDevice->Release();
 	pAdapter->Release();
 	pFactory->Release();
+
+	return S_OK;
+}
+
+//new
+int CDevice::CreateView()
+{
+	// 1. RenderTarget Texture를 스왚체인으로부터 가져오기
+	m_SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&m_RenderTargetTex);
+
+	// 2. RenderTargetView 를 생성한다.
+	m_Device->CreateRenderTargetView(m_RenderTargetTex, nullptr, &m_RTV);
+
+	// 3. DepthStencil 용 Texture 를 제작
+	D3D11_TEXTURE2D_DESC Desc = {};
+
+	Desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	Desc.Width = (UINT)m_RenderResolution.x;
+	Desc.Height = (UINT)m_RenderResolution.y;
+	Desc.ArraySize = 1;
+
+	Desc.CPUAccessFlags = 0;
+	Desc.Usage = D3D11_USAGE_DEFAULT;
+	Desc.MipLevels = 1;
+	Desc.SampleDesc.Count = 1;
+	Desc.SampleDesc.Quality = 0;
+	Desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+
+	m_Device->CreateTexture2D(&Desc, nullptr, &m_DepthStencilTex);
+
+	// 4. DepthStencil View
+	m_Device->CreateDepthStencilView(m_DepthStencilTex, nullptr, &m_DSV);
+
+	// RenderTarget 과  DepthStencilTarget 을 출력으로 지정한다.
+	m_Context->OMSetRenderTargets(1, &m_RTV, m_DSV);
 
 	return S_OK;
 }
--- DirectX11Engine/Project/Engine/CDevice.h
+++ DirectX11Engine/Project/Engine/CDevice.h
@@ -3,19 +3,19 @@
 class CDevice
 {
 private:
-	HWND					m_hMainWnd;				// 출력대상 윈도우
-	POINT					m_RenderResolution;		// 랜더링 타겟의 해상도
+	HWND m_hMainWnd; // 출력대상 윈도우
+	POINT m_RenderResolution; // 랜더링 타겟의 해상도
 
-	ID3D11Device*			m_Device;				// GPU 메모리 할당, Dx11 관련 객체 생성
-	ID3D11DeviceContext*	m_Context;				// GPU 렌더링 관련 명령
+	ID3D11Device* m_Device; // GPU 메모리 할당, Dx11 관련 객체 생성
+	ID3D11DeviceContext* m_Context; // GPU 렌더링 관련 명령
 
-	IDXGISwapChain*			m_SwapChain;			// 렌더타겟 버퍼 소유, 화면에 최종 장면을 게시
+	IDXGISwapChain* m_SwapChain; // 렌더타겟 버퍼 소유, 화면에 최종 장면을 게시
 	
-	ID3D11Texture2D*		m_RenderTargetTex;
+	ID3D11Texture2D* m_RenderTargetTex;
 	ID3D11RenderTargetView* m_RTV;
 
-	ID3D11Texture2D*		m_DepthStencilTex;
-	ID3D11RenderTargetView*	m_DSV;
+	ID3D11Texture2D* m_DepthStencilTex;
+	ID3D11DepthStencilView*	m_DSV;
 
 public:
 	static CDevice* GetInst()
@@ -29,6 +29,8 @@ public:
 
 private:
 	int CreateSwapchain();
+	//new
+	int CreateView();
 
 private:
 	CDevice();
