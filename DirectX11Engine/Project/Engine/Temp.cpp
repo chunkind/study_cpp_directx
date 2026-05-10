9cc9d072f5813f0dba3cd4e67073bb41a40c6537
chunkind <chunkind@naver.com>
2026-05-10 18:26:11 +0900

    5/10: 그래픽스 파이프라인 Temp

---
+++ DirectX11Engine/Project/Engine/Temp.cpp
@@ -0,0 +1,64 @@
+#include "pch.h"
+#include "Temp.h"
+
+// * Graphics Pipeline
+
+// ======================
+// IA (Input Assembler)
+// ======================
+// Vertext Buffer
+// Index Buffer
+// Topology
+// Layout
+
+// ======================
+// Vertex Shader Stage
+// ======================
+// 정점을 2차원 모델 좌표계에서 NDC 2차원 좌표로 맵핑
+
+// Tessellation
+// = Hull Shader
+// - Domain Shader
+
+// Geometry Shader
+
+// ======================
+// Resterizer
+// ======================
+
+// ======================
+// Pixel Shader
+// ======================
+// 픽셀 당 호출되는 함수
+// 각 픽셀의 색상을 지정해서 렌더타겟에 출력
+
+// ======================
+// Output Merge State
+// ======================
+// DepthStencil State
+// BlendState State
+
+// 최종 출력
+// RenderTargetTexture
+// DepthStencilTexture
+
+
+void TempInit()
+{
+
+}
+
+void TempRelease()
+{
+
+}
+
+void TempTick()
+{
+
+}
+
+void TempRender()
+{
+
+}
---
+++ DirectX11Engine/Project/Engine/Temp.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void TempInit();
+
+void TempRelease();
+
+void TempTick();
+
+void TempRender();
